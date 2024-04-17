9c8ed5ec01bdf738f51aff1623b648ea62664510
nlee113 <82241838+nlee113@users.noreply.github.com>
2024-04-17 09:14:50 -0700

    bg image, updated draw function

--- js/sprite.js
+++ js/sprite.js
@@ -1,4 +1,4 @@
-//Parent Sprit Classa
+//Parent Sprit Class
 class Sprite {
     constructor(sprite_json, x, y, start_state){
         this.sprite_json = sprite_json;
@@ -8,35 +8,33 @@ class Sprite {
         this.root_e = "poke";
 
         this.cur_frame = 0;
-        this.frameCount = this.sprite_json[this.root_e][this.state].length;
+
+        this.cur_bk_data = null;
 
         this.x_v = 10;
         this.y_v = 0;
 
-        this.loadImage();
-    }
-
-    loadImage() {
-        const frameData = this.sprite_json[this.root_e][this.state][this.cur_frame];
-        this.img = new Image();
-        this.img.src = 'sprite_girl/' + this.root_e + '/' + this.state + '/' + this.cur_frame + '.png';
-        this.img.onload = () => {
-            this.draw();
-        };
-        this.img.onerror = (error) => {
-            console.error("Error loading image:", error);
-        };
     }
 
     draw(){
         var ctx = canvas.getContext('2d');
-        //console.log(this.sprite_json[this.root_e][this.state][this.cur_frame]['w']);
 
+        //console.log(this.sprite_json[this.root_e][this.state][this.cur_frame]['w']);
         if(this.sprite_json[this.root_e][this.state][this.cur_frame]['img'] == null){
             console.log("loading");
             this.sprite_json[this.root_e][this.state][this.cur_frame]['img'] = new Image();
             this.sprite_json[this.root_e][this.state][this.cur_frame]['img'].src = 'sprite_girl/' + this.root_e + '/' + this.state + '/' + this.cur_frame + '.png';
         }
+
+        if( this.cur_bk_data != null){
+            ctx.putImageData(this.cur_bk_data , (this.x - this.x_v) , (this.y - this.y_v));
+        }
+
+        this.cur_bk_data = ctx.getImageData(this.x, this.y, 
+                        this.sprite_json[this.root_e][this.state][this.cur_frame]['w'], 
+                        this.sprite_json[this.root_e][this.state][this.cur_frame]['h']);
+
+
         ctx.drawImage(this.sprite_json[this.root_e][this.state][this.cur_frame]['img'], this.x, this.y );
 
         this.cur_frame = this.cur_frame + 1;
@@ -45,17 +43,32 @@ class Sprite {
             this.cur_frame = 0;
         }
 
-        this.x = this.x + this.x_v;
-        this.y = this.y + this.y_v;
-
-        if(this.x >= window.innerWidth){
-            this.x_v = -10;
-            this.state = 'walk_W';
+        if(this.x >= (window.innerWidth - this.sprite_json[this.root_e][this.state][this.cur_frame]['w']) ){
+            this.bound_hit('E');
+        }else if(this.x <= 0){
+            this.bound_hit('W');
+        }else if(this.y >= (window.innerHeight - this.sprite_json[this.root_e][this.state][this.cur_frame]['h']) ){
+            this.bound_hit('S');
+        }else if(this.y <= 0){
+            this.bound_hit('N');
+        }else{
+            this.x = this.x + this.x_v;
+            this.y = this.y + this.y_v;
         }
 
-        if(this.x <= 0){
-            this.x_v = 10;
-        }
-        
     }
+
+    set_idle_state(){
+        this.x_v = 0;
+        this.y_v = 0;
+        const idle_state = ["idle","idleBackAndForth","idleBreathing","idleFall","idleLayDown","idleLookAround","idleLookDown","idleLookLeft","idleLookRight","idleLookUp","idleSit","idleSpin","idleWave"];
+
+        const random = Math.floor(Math.random() * idle_state.length);
+        console.log(idle_state[random]);
+        this.state = idle_state[random];
+    }
+
+    bound_hit(side){
+            this.set_idle_state();
+   }
 }
