eabb636d48cb06c252157bd4ee7500bb0e0b601d
nlee113 <82241838+nlee113@users.noreply.github.com>
2024-04-21 23:45:49 -0700

    more animations and fixes

--- js/sprite.js
+++ js/sprite.js
@@ -14,13 +14,60 @@ class Sprite {
         this.x_v = 10;
         this.y_v = 0;
 
+        this.bound_x = 0;
+        this.bound_y = 0;
+        this.idle_state = ["idle","idleSpin","idleMagic"];
+        this.in_idle = 0;
     }
 
-    draw(state){
+    velocity() {
+        if (this.state == "walk_N") {
+            this.x_v = 0;
+            this.y_v = -7;
+        }
+        else if (this.state == "walk_E") {
+            this.x_v = 7;
+            this.y_v = 0;
+        }
+        else if (this.state == "walk_S") {
+            this.x_v = 0;
+            this.y_v = 7;
+        }
+        else if (this.state == "walk_W") {
+            this.x_v = -7;
+            this.y_v = 0;
+        }
+        else {
+            this.x_v = 0;
+            this.y_v = 0;
+        }
+
+    }
+
+    draw(new_state){
         var ctx = canvas.getContext('2d');
 
+        if( this.cur_bk_data != null){
+            ctx.putImageData(this.cur_bk_data , (this.x - (this.x_v + this.bound_x)) , (this.y - (this.y_v + this.bound_y)));
+            this.bound_x = 0
+            this.bound_y = 0
+        }
+
         //console.log(this.sprite_json[this.root_e][this.state][this.cur_frame]['w']);
-        console.log(state['key_change']);
+        if (this.idle_state.includes(new_state)) {
+            if (this.in_idle == 0) {
+                this.set_idle_state();
+            }
+        }
+        else {
+            this.state = new_state;
+            this.in_idle = 0;
+        }
+
+        if (this.cur_frame >= this.sprite_json[this.root_e][this.state].length) {
+            this.cur_frame = 0;
+        }
+        this.velocity();
 
         if(this.sprite_json[this.root_e][this.state][this.cur_frame]['img'] == null){
             console.log("loading");
@@ -28,61 +75,50 @@ class Sprite {
             this.sprite_json[this.root_e][this.state][this.cur_frame]['img'].src = 'sprite_girl/' + this.root_e + '/' + this.state + '/' + this.cur_frame + '.png';
         }
 
-        if( this.cur_bk_data != null){
-            ctx.putImageData(this.cur_bk_data , (this.x - this.x_v) , (this.y - this.y_v));
-        }
-
         this.cur_bk_data = ctx.getImageData(this.x, this.y, 
-                        this.sprite_json[this.root_e][this.state][this.cur_frame]['w'], 
-                        this.sprite_json[this.root_e][this.state][this.cur_frame]['h']);
-
+                        this.sprite_json[this.root_e][this.state][this.cur_frame]['w'] + 5, 
+                        this.sprite_json[this.root_e][this.state][this.cur_frame]['h'] + 5);
 
         ctx.drawImage(this.sprite_json[this.root_e][this.state][this.cur_frame]['img'], this.x, this.y );
-
         this.cur_frame = this.cur_frame + 1;
+
         if(this.cur_frame >= this.sprite_json[this.root_e][this.state].length){
-            console.log(this.cur_frame);
             this.cur_frame = 0;
         }
 
         if(this.x >= (window.innerWidth - this.sprite_json[this.root_e][this.state][this.cur_frame]['w']) ){
+            this.x--;
+            this.bound_x = -1;
             this.bound_hit('E');
         }else if(this.x <= 0){
+            this.x++;
+            this.bound_x = 1;
             this.bound_hit('W');
         }else if(this.y >= (window.innerHeight - this.sprite_json[this.root_e][this.state][this.cur_frame]['h']) ){
+            this.y--;
+            this.bound_y = -1;
             this.bound_hit('S');
         }else if(this.y <= 0){
+            this.y++;
+            this.bound_y = 1;
             this.bound_hit('N');
         }else{
             this.x = this.x + this.x_v;
             this.y = this.y + this.y_v;
         }
-        return false;
     }
 
     set_idle_state(){
+        if(this.in_idle == 0)
         this.x_v = 0;
         this.y_v = 0;
-        const idle_state = ["idle"];
+        const random = Math.floor(Math.random() * this.idle_state.length);
+        this.state = this.idle_state[random];
+        this.cur_frame = 0;
+        this.in_idle = 1;
     }
 
     bound_hit(side){
             this.set_idle_state();
    }
-    handleKeyDown(event) {
-        switch(event.key) {
-            case 'ArrowUp':
-                this.y_v = -10;
-                break;
-            case 'ArrowDown':
-                this.y_v = 10;
-                break;
-            case 'ArrowLeft':
-                this.x_v = -10;
-                break;
-            case 'ArrowRight':
-                this.x_v = 10;
-                break;
-        }
-    }
 }
